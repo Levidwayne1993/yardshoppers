bbce2f155cf58f9c8f8d648d9bb638d8da2e28ea
Levidwayne1993 <dpzking93@gmail.com>
2026-04-27 15:28:03 -0700

    add AdminFloatingBar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import PageViewTracker from "@/components/PageViewTracker";
 import CookieConsent from "@/components/CookieConsent";
+import AdminFloatingBar from "@/components/AdminFloatingBar";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -253,6 +254,7 @@ export default function RootLayout({
 
         <PageViewTracker />
         <Navbar />
+        <AdminFloatingBar />
         <main className="flex-1">{children}</main>
         <Footer />
         <CookieConsent />
